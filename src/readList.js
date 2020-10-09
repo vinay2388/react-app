4e0970e2fc4b1f82703e08c3c76bc6f0deb235ec
vinay.reddy <vinay.reddy@stackaero.com>
2020-10-09 16:01:08 +0530

    updated

--- src/readList.js
+++ src/readList.js
@@ -16,7 +16,7 @@ class ReadList extends Component {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpbG9jYWwiLCJ1c2VySWQiOiI1ZjFlNjBhZDViMTMxODAxOWRkZjY0YTQiLCJ0ZW5hbnRJZCI6IjVjZTY3Yzg1ZWU0ZmEyMDAyZjhlNDEwOCIsIm9yZ0lkIjoiNWNlNjdhNjllZTRmYTIwMDJmOGUwZjEwIiwiaWF0IjoxNjAyMDU5NTA1LCJleHAiOjE2MDIwODgzMDV9.jZqqFk4FLC-EKdadWWulGx4A14W-QnPsaM_7kLVXlvq-pRwYQZhFXpcKKKOz2nBLpTgbMtvufz9BhjFy8eMLe1ejk2Yt8G4xtGjKdBEkMX_OYxOhZObFutYksT7sfPHUdYz8eml7qpU7DGovXziF9qLbZ1yBntr7ZGOz0XEEp2-43urm2pt89WSBG7TIeWJL8FzjWlGZnC-hJsBabPkMeudP_NSUk6BNFdlepFD7EPnjIrrbuSGfVYtzklrvT4XXQW_nBT_3p4423epgdtruVGpc4w3dt5dWTtQrB7cLiPjWxX0FoQLQH42qaUMS27qS5G40O46-htuJb5wCyxIcYA',
+                'Authorization': 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpbG9jYWwiLCJ1c2VySWQiOiI1ZjFlNjBhZDViMTMxODAxOWRkZjY0YTQiLCJ0ZW5hbnRJZCI6IjVjZTY3Yzg1ZWU0ZmEyMDAyZjhlNDEwOCIsIm9yZ0lkIjoiNWNlNjdhNjllZTRmYTIwMDJmOGUwZjEwIiwiaWF0IjoxNjAyMjIwMjU4LCJleHAiOjE2MDIyNDkwNTh9.LmlPXEiTXcHknHbIh-uasRu5fmXuOnOQYPM3x7Ed65ZgRfY5KH9piTHZnSbpLXPLFcTObWz0O4kJ2gOyD1S_IIWPRWQIERLnhed-cGZCTDcFRUfFw9bUpuXcDu1Ag6Rp_qegaTKqRtZs5sWzeL27rO1qiUJ-vU1HhZ7z6bBNFC4Y7z0LzvQOK89Tvma79N8PDOEGYg4VqQgaiqo5w0LcBq8dx96XlyiFhvhdSS4wdtIciA7_GDAYfRBuH_bnt1Qxx7ZDbNoxmRYH0pcOu9GTy-43DDy3DgJ3Lsy_BppWlmQcXnZGKDoP6rB5pTXbJjH8zCFWcHlo4eRIWwKSqcPkvg',
             }
         };
         fetch(`${process.env.REACT_APP_API_URL}/api/categories/`, requestOptions)
@@ -30,7 +30,7 @@ class ReadList extends Component {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpbG9jYWwiLCJ1c2VySWQiOiI1ZjFlNjBhZDViMTMxODAxOWRkZjY0YTQiLCJ0ZW5hbnRJZCI6IjVjZTY3Yzg1ZWU0ZmEyMDAyZjhlNDEwOCIsIm9yZ0lkIjoiNWNlNjdhNjllZTRmYTIwMDJmOGUwZjEwIiwiaWF0IjoxNjAyMDU5NTA1LCJleHAiOjE2MDIwODgzMDV9.jZqqFk4FLC-EKdadWWulGx4A14W-QnPsaM_7kLVXlvq-pRwYQZhFXpcKKKOz2nBLpTgbMtvufz9BhjFy8eMLe1ejk2Yt8G4xtGjKdBEkMX_OYxOhZObFutYksT7sfPHUdYz8eml7qpU7DGovXziF9qLbZ1yBntr7ZGOz0XEEp2-43urm2pt89WSBG7TIeWJL8FzjWlGZnC-hJsBabPkMeudP_NSUk6BNFdlepFD7EPnjIrrbuSGfVYtzklrvT4XXQW_nBT_3p4423epgdtruVGpc4w3dt5dWTtQrB7cLiPjWxX0FoQLQH42qaUMS27qS5G40O46-htuJb5wCyxIcYA',
+                'Authorization': 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpbG9jYWwiLCJ1c2VySWQiOiI1ZjFlNjBhZDViMTMxODAxOWRkZjY0YTQiLCJ0ZW5hbnRJZCI6IjVjZTY3Yzg1ZWU0ZmEyMDAyZjhlNDEwOCIsIm9yZ0lkIjoiNWNlNjdhNjllZTRmYTIwMDJmOGUwZjEwIiwiaWF0IjoxNjAyMjIwMjU4LCJleHAiOjE2MDIyNDkwNTh9.LmlPXEiTXcHknHbIh-uasRu5fmXuOnOQYPM3x7Ed65ZgRfY5KH9piTHZnSbpLXPLFcTObWz0O4kJ2gOyD1S_IIWPRWQIERLnhed-cGZCTDcFRUfFw9bUpuXcDu1Ag6Rp_qegaTKqRtZs5sWzeL27rO1qiUJ-vU1HhZ7z6bBNFC4Y7z0LzvQOK89Tvma79N8PDOEGYg4VqQgaiqo5w0LcBq8dx96XlyiFhvhdSS4wdtIciA7_GDAYfRBuH_bnt1Qxx7ZDbNoxmRYH0pcOu9GTy-43DDy3DgJ3Lsy_BppWlmQcXnZGKDoP6rB5pTXbJjH8zCFWcHlo4eRIWwKSqcPkvg',
             }
         };
         fetch(`${process.env.REACT_APP_API_URL}/api/categories/` + data._id, requestOptions)
@@ -52,7 +52,7 @@ class ReadList extends Component {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpbG9jYWwiLCJ1c2VySWQiOiI1ZjFlNjBhZDViMTMxODAxOWRkZjY0YTQiLCJ0ZW5hbnRJZCI6IjVjZTY3Yzg1ZWU0ZmEyMDAyZjhlNDEwOCIsIm9yZ0lkIjoiNWNlNjdhNjllZTRmYTIwMDJmOGUwZjEwIiwiaWF0IjoxNjAyMDU5NTA1LCJleHAiOjE2MDIwODgzMDV9.jZqqFk4FLC-EKdadWWulGx4A14W-QnPsaM_7kLVXlvq-pRwYQZhFXpcKKKOz2nBLpTgbMtvufz9BhjFy8eMLe1ejk2Yt8G4xtGjKdBEkMX_OYxOhZObFutYksT7sfPHUdYz8eml7qpU7DGovXziF9qLbZ1yBntr7ZGOz0XEEp2-43urm2pt89WSBG7TIeWJL8FzjWlGZnC-hJsBabPkMeudP_NSUk6BNFdlepFD7EPnjIrrbuSGfVYtzklrvT4XXQW_nBT_3p4423epgdtruVGpc4w3dt5dWTtQrB7cLiPjWxX0FoQLQH42qaUMS27qS5G40O46-htuJb5wCyxIcYA',
+                'Authorization': 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpbG9jYWwiLCJ1c2VySWQiOiI1ZjFlNjBhZDViMTMxODAxOWRkZjY0YTQiLCJ0ZW5hbnRJZCI6IjVjZTY3Yzg1ZWU0ZmEyMDAyZjhlNDEwOCIsIm9yZ0lkIjoiNWNlNjdhNjllZTRmYTIwMDJmOGUwZjEwIiwiaWF0IjoxNjAyMjIwMjU4LCJleHAiOjE2MDIyNDkwNTh9.LmlPXEiTXcHknHbIh-uasRu5fmXuOnOQYPM3x7Ed65ZgRfY5KH9piTHZnSbpLXPLFcTObWz0O4kJ2gOyD1S_IIWPRWQIERLnhed-cGZCTDcFRUfFw9bUpuXcDu1Ag6Rp_qegaTKqRtZs5sWzeL27rO1qiUJ-vU1HhZ7z6bBNFC4Y7z0LzvQOK89Tvma79N8PDOEGYg4VqQgaiqo5w0LcBq8dx96XlyiFhvhdSS4wdtIciA7_GDAYfRBuH_bnt1Qxx7ZDbNoxmRYH0pcOu9GTy-43DDy3DgJ3Lsy_BppWlmQcXnZGKDoP6rB5pTXbJjH8zCFWcHlo4eRIWwKSqcPkvg',
             },
             body: JSON.stringify(data)
         };
